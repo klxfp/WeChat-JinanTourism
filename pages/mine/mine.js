ee1beecb1816ca064f683f718a4c3fe87e7fc49d
曹建萍 <370324232@qq.com>
2019-12-05 21:49:20 +0800

    mine修改
    未完成修改

--- pages/mine/mine.js
+++ pages/mine/mine.js
@@ -4,6 +4,11 @@ var app = getApp();
 Page({
     data:{
         userInfo:{},
+        swiperImg: [
+          { src: "https://dimg06.c-ctrip.com/images/100m11000000rhzcqD3D4_R_1600_10000_Mtg_7.jpg" },
+          { src: "https://dimg08.c-ctrip.com/images/100l0y000000m1ilk9C71_R_1600_10000_Mtg_7.jpg" },
+          { src: "https://dimg04.c-ctrip.com/images/100w10000000ph6twC31B_R_1600_10000_Mtg_7.jpg" }
+        ],
         menuList:[
             {
                 list:[
@@ -39,11 +44,20 @@ Page({
     onLoad:function(){
         var that = this;
         // 调用应用实例的方法获取全局数据
-        app.getUserInfo(function(userInfo){
-            // 更新数据
-            that.setData({
-                userInfo:userInfo
-            })
-        });
-    }
+        // app.getUserInfo(function(userInfo){
+        //     // 更新数据
+        //     that.setData({
+        //         userInfo:userInfo
+        //     })
+        // });
+        this.setData({
+          userInfo: getApp().globalData.userInfo
+        })
+        if (getApp().globalData.userInfo.avatarUrl) {
+          console.log(getApp().globalData.userInfo.avatarUrl)
+          this.setData({
+            headerImg: getApp().globalData.userInfo.avatarUrl
+          })
+        }
+      }
 })
