1432bb890339b6c1ccbb0b6ff70429c15eea4ce0
张金山 <201921200707>
2019-12-06 12:44:28 +0800

    调整首页图标等
    图片尺寸待调整
    字体样式待调整

--- pages/index/index.js
+++ pages/index/index.js
@@ -8,91 +8,139 @@ var _url = app.globalData.url;
 
 var amapFile = require('../../utils/amap-wx.js');
 // 实例化地图API，获取当前定位信息
-var myAmapFun = new amapFile.AMapWX({key:'fa5b1bf50930615d2303c0072aa33691'});
+var myAmapFun = new amapFile.AMapWX({
+  key: 'fa5b1bf50930615d2303c0072aa33691'
+});
 
 Page({
-    data:{
-        cityName:'',       
-        hotList:null,
-        navList:[
-            {
-                name:"名优特产",
-                icon:"btn_specialty.png",
-                page:"local-product/index"
-            },
-            {
-                name:"名店小吃",
-                icon:"btn_shop.png",
-                page:"local-snack/local-snack"
-            },
-            {
-                name:"行程管理",
-                icon:"btn_journey.png",
-                page:""
-            },
-            {
-                name:"景点门票",
-                icon:"btn_ticket.png",
-                page:""
-            }
-        ]
-    },
-    onReady:function(){
-        var that = this;
-        // 调用高德地图接口，获取城市信息
-        if(app.globalData.curCity){
-             this.setData({cityName:app.globalData.curCity});
-             this.getHot();
-        }else{
-            myAmapFun.getRegeo({
-                success: function(data){
-                    //获取当前城市信息
-                    
-                    console.log(data[0].regeocodeData.addressComponent);
-                    var _msg = data[0].regeocodeData.addressComponent;
-                    var _city = _msg.city[0]?_msg.city[0]:_msg.province;
-                    app.globalData.curCity = _city;
-                    that.setData({
-                        cityName:_city
-                    });
+  data: {
+    cityName: '',
+    hotList: null,
+    navList: [{
+        name: "名店特产",
+        icon: "icon_4.png",
+        page: "local-product/index"
+      },
+      {
+        name: "景区讲解",
+        icon: "icon_1.png",
+        page: "local-snack/local-snack"
+      },
+      {
+        name: "目的地",
+        icon: "icon_2.png",
+        page: ""
+      },
+      {
+        name: "景点门票",
+        icon: "btn_ticket.png",
+        page: ""
+      }
+    ],
+    imgList: [
+      'https://s2.ax1x.com/2019/12/06/QJlMq0.jpg',
+      'https://s2.ax1x.com/2019/12/06/QJlJG4.jpg',
+      'https://s2.ax1x.com/2019/12/06/QJlqQs.jpg',
+    ],
+    circular: true,
+    //是否显示画板指示点  
+    indicatorDots: false,
+    //选中点的颜色  
+    indicatorcolor: "#000",
+    //是否竖直  
+    vertical: false,
+    //是否自动切换  
+    autoplay: true,
+    //自动切换的间隔
+    interval: 2500,
+    //滑动动画时长毫秒  
+    duration: 100,
+    //所有图片的高度  
+    imgheights: [],
+    //图片宽度 
+    imgwidth: 750,
+    //默认  
+    current: 0
 
-                    // console.log(app.globalData.curCity);
+  },
+  onReady: function() {
+    var that = this;
+    // 调用高德地图接口，获取城市信息
+    if (app.globalData.curCity) {
+      this.setData({
+        cityName: app.globalData.curCity
+      });
+      this.getHot();
+    } else {
+      myAmapFun.getRegeo({
+        success: function(data) {
+          //获取当前城市信息
 
-                    // 获取当前城市热门景点列表
-                    that.getHot();
-                },
-                fail: function(info){
-                    //失败回调
-                    console.log(info)
-                }
-            });
+          console.log(data[0].regeocodeData.addressComponent);
+          var _msg = data[0].regeocodeData.addressComponent;
+          var _city = _msg.city[0] ? _msg.city[0] : _msg.province;
+          app.globalData.curCity = _city;
+          that.setData({
+            cityName: _city
+          });
+
+          // console.log(app.globalData.curCity);
+
+          // 获取当前城市热门景点列表
+          that.getHot();
+        },
+        fail: function(info) {
+          //失败回调
+          console.log(info)
         }
-        
-    },
-    myLocation:function(){
-        var that = this;
-        wx.getLocation({
-            // 返回可以用于wx.openLocation的经纬度
-            type:'gcj02',
-            success:function(res){
-                var latitude = res.latitude;
-                var longitude = res.longitude;
-                wx.openLocation({
-                    latitude:latitude,
-                    longitude:longitude,
-                    scale:1
-                })
-            }
-        });
-    },
-    getHot:function () {
-        var that = this;  
-        ajax.post(_url,
-                'cityName='+that.data.cityName+'&lanKey=zh-cn&provinceName=&method=scenicsOfCityNew&',
-                function(res){
-                    that.setData({
-                        hotList:res.data.data.scenics
-                    });
-                })
+      });
     }
+
+  },
+  myLocation: function() {
+    var that = this;
+    wx.getLocation({
+      // 返回可以用于wx.openLocation的经纬度
+      type: 'gcj02',
+      success: function(res) {
+        var latitude = res.latitude;
+        var longitude = res.longitude;
+        wx.openLocation({
+          latitude: latitude,
+          longitude: longitude,
+          scale: 1
+        })
+      }
+    });
+  },
+  getHot: function() {
+    var that = this;
+    ajax.post(_url,
+      'cityName=' + that.data.cityName + '&lanKey=zh-cn&provinceName=&method=scenicsOfCityNew&',
+      function(res) {
+        that.setData({
+          hotList: res.data.data.scenics
+        });
+      })
+  },
+  imageLoad: function (e) {//获取图片真实宽度  
+    var imgwidth = e.detail.width,
+      imgheight = e.detail.height,
+      //宽高比  
+      ratio = imgwidth / imgheight;
+    console.log(imgwidth, imgheight)
+    //计算的高度值  
+    var viewHeight = 750 / ratio;
+    var imgheight = viewHeight;
+    var imgheights = this.data.imgheights;
+    //把每一张图片的对应的高度记录到数组里  
+    imgheights[e.target.dataset.id] = imgheight;
+    this.setData({
+      imgheights: imgheights
+    })
+  },
+  bindchange: function (e) {
+    // console.log(e.detail.current)
+    this.setData({ current: e.detail.current })
+  },
 })
