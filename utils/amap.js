380f845147379e0326457e16e710da4c16628cc6
姜琳琳 <309958052@qq.com>
2019-12-08 21:47:23 +0800

    地图配置文件

--- utils/amap.js
+++ utils/amap.js
@@ -11,7 +11,6 @@ class Amap {
   static getPoiAround(querykeywords = '') {
     return new Promise((resolve, reject) => myAmapFun.getPoiAround({ querykeywords, success: resolve, fail: reject }));
   }
-
   /**
    * 获取地理描述数据
    */
