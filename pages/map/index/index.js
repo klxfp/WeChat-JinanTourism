0bc1abafa5d4236f708000e779ec462204ee178b
jianglinlin <309958052@qq.com>
2019-12-11 10:07:43 +0800

    地图-美食功能实现

--- pages/map/index/index.js
+++ pages/map/index/index.js
@@ -134,5 +134,30 @@ Page({
         wx.showModal({ title: info.errMsg })
       }
     })
+  },
+  clickFood: function () {
+    var that = this;
+    var myAmapFun = new amapFile.AMapWX({ key: '6205e3022b70167945e90fec43976555' });
+    myAmapFun.getPoiAround({
+      iconPathSelected: '../images/Redmaker.png',
+      iconPath: '../images/Bluemaker.png',
+      querykeywords: '美食',
+      success: function (data) {
+        markersData = data.markers;
+        that.setData({
+          markers: markersData
+        });
+        that.setData({
+          latitude: markersData[0].latitude
+        });
+        that.setData({
+          longitude: markersData[0].longitude
+        });
+        that.showMarkerInfo(markersData, 0);
+      },
+      fail: function (info) {
+        wx.showModal({ title: info.errMsg })
+      }
+    })
   }
 })
