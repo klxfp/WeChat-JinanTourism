53fbaff3c6cd68954c5b466d049c74fb40d4e7dc
张金山 <201921200707>
2019-12-19 03:06:45 +0800

    插件怎么又出问题了

--- pages/audio/scenic/s1/s1.js
+++ pages/audio/scenic/s1/s1.js
@@ -1,10 +1,13 @@
+const app = getApp();
+//引入插件：微信同声传译
+const plugin = requirePlugin('WechatSI');
+
 var startPoint;
 //引入本地json数据，这里引入的就是第一步定义的json数据
 var jsonData = require('../json.js');
 
-console.log(jsonData.dataList[0])
-console.log(jsonData.dataList[0].name)
-
+console.log(jsonData.dataList[0]);
+console.log(jsonData.dataList[0].name);
 
 
 
@@ -17,13 +20,27 @@ Page({
     imgurl: "../../../../images/icon_play.png",
     isShow: true,
     isMove: false,
-    isChange: false
+    isChange: false,
+    content: '请先点击文字选择解说内容。', //内容
+    src: ''
+
   },
-  onLoad: function(options) {
-    
+  onReady(e) {
+    //创建内部 audio 上下文 InnerAudioContext 对象。
+    this.innerAudioContext = wx.createInnerAudioContext();
+    this.innerAudioContext.onError(function (res) {
+      console.log(res);
+      wx.showToast({
+        title: '语音播放失败',
+        icon: 'none',
+      })
+    })
+  },
+  onLoad: function (options) {
+
     var that = this;
     wx.getSystemInfo({
-      success: function(res) {
+      success: function (res) {
         console.log(res);
         // 屏幕宽度、高度
         console.log('height=' + res.windowHeight);
@@ -40,19 +57,46 @@ Page({
     })
   },
 
-  onShow: function() {
+  onShow: function () {
 
   },
+  setContent: function (e) {
+    console.log(e.currentTarget.dataset.text)
+    this.setData({
+      content: e.currentTarget.dataset.text,
+    })
+  },
+  // 文字转语音
+  wordYun: function (e) {
+    var that = this;
+    var content = this.data.content;
+    plugin.textToSpeech({
+      lang: "zh_CN",
+      tts: true,
+      content: content,
+      success: function (res) {
+        console.log(res);
+        console.log("succ tts", res.filename);
+        that.setData({
+          src: res.filename
+        })
+        that.yuyinPlay();
 
+      },
+      fail: function (res) {
+        console.log("fail tts", res)
+      }
+    })
+  },
 
-  viewStart: function(e) {
+  viewStart: function (e) {
     startPoint = e.touches[0]
     console.log("start")
 
   },
-  viewMove: function(e) {
+  viewMove: function (e) {
     console.log("move")
-  
+
     var endPoint = e.touches[e.touches.length - 1]
     var translateX = endPoint.clientX - startPoint.clientX
     var translateY = endPoint.clientY - startPoint.clientY
@@ -78,47 +122,57 @@ Page({
       isMove: true
     })
   },
-  viewEnd: function(e) {
+  viewEnd: function (e) {
     console.log("end")
-    if (this.data.isMove==true){
+    if (this.data.isMove == true) {
       console.log("动了")
       this.setData({
         isMove: false
       })
-    }else{
+    } else {
       console.log("没动")
-      if (this.data.isChange==false){
+      if (this.data.isChange == false) {
         this.setData({
           isChange: true,
           imgurl: "../../../../images/icon_stop.png",
-        })
+        });
         // 播放事件在此写
         console.log("播放")
-      }else{
+        if (this.data.src == '') {
+          console.log(暂无语音);
+          return;
+        }
+        console.log("??")
+        this.innerAudioContext.src = this.data.src //设置音频地址
+        this.innerAudioContext.play(); //播放音频
+        console.log("播放ok")
+      } 
+      else {
         this.setData({
           isChange: false,
           imgurl: "../../../../images/icon_play.png",
-        })
+        });
         // 停止事件在此写
+        this.innerAudioContext.pause(); //暂停音频
         console.log("停止")
       }
 
     }
 
   },
-  close_tap: function() {
+  close_tap: function () {
     this.setData({
       isShow: true
     })
   },
 
-  open_tap: function() {
+  open_tap: function () {
     this.setData({
       isShow: false
     })
   },
 
-  open: function(res) {
+  open: function (res) {
     console.log(res);
     let url
     if (res.currentTarget.dataset.msg == "../../../../images/icon_play.png") {
