b28c057001629f10821c1cfcb9a626d5ff213a6b
张金山 <201921200707>
2019-12-19 00:38:42 +0800

    欲言又止

--- pages/audio/scenic/s2/s2.js
+++ pages/audio/scenic/s2/s2.js
@@ -1,4 +1,8 @@
 // pages/audio/scenic/s2/s2.js
+
+//云数据库初始化
+const db = wx.cloud.database({});
+const cont = db.collection('books');
 Page({
 
   /**
@@ -12,13 +16,21 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    console.log("11")
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
+    console.log("22")
+    db.collection('book').get({
+      success(res) {
+        console.log("******")
+        console.log(res.data)
+      }
+    })
+
 
   },
 
@@ -26,6 +38,7 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
+    console.log("33")
 
   },
 
@@ -33,34 +46,34 @@ Page({
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-
+    console.log("55")
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-
+    console.log("66")
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
+    console.log("77")
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-
+    console.log("88")
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-
+    console.log("99")
   }
 })
