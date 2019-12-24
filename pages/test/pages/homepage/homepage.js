33b55e03cf4ab3329c2dae2d0a166efca12d58c0
曹建萍 <370324232@qq.com>
2019-12-24 11:35:23 +0800

    首页面中景点门票更改

--- pages/test/pages/homepage/homepage.js
+++ pages/test/pages/homepage/homepage.js
@@ -61,118 +61,118 @@ var tabNames = ["recommand", "nearby"];
         });
       }
     },
-    getLocationCity: function(hasCurrentSelectCity, callback) {
-      var _this = this,
-          _api = api;
-      var qqMap = app.getQQMap();
-      var _app = app;
-      var lat, lng;
-      api.Location.get({
-          success: function(res) {
-              __wxConfig.debug && console.log("homepage Location res = ", res)
-              lat = res.latitude;
-              lng = res.longitude;
-              qqMap.reverseGeocoder({
-                  location: {
-                      latitude: res.latitude,
-                      longitude: res.longitude
-                  },
-                  success: function(res) {
-                      __wxConfig.debug && console.log("homepage qq map reverseGeocoder success res = ",res);
-                      //遍历城市解析出城市id
-                      var currentCityName = res.result.address_component.city//北京市
-                      __wxConfig.debug && console.log("homepage qq map reverseGeocoder success currentCityName = ",currentCityName);
-                      _app.getSavedCityData(function(result){
-                          __wxConfig.debug && console.log("homepage qq map reverseGeocoder success getSavedCityData  result = ",result);
-                          if(result && result.data && !Util.isEmptyObject(result.data)) {
-                              var city = _this.getCityByName(currentCityName.replace(/市/,''), result.data.cityList);
-                              isChinaCity = true;
-                              if (Util.isEmptyObject(city)) {
-                                isChinaCity = false;
-                                city = _this.getCityByName(currentCityName.replace(/市/,''), result.data.overseasCityList);
-                              }
-                              __wxConfig.debug && console.log("homepage qq map reverseGeocoder success getSavedCityData  city = ",city);
-                              if(city && city.cityId) {
-                                  showLocationDailog = city.cityId != _this.data.searchCityId;
-                                  _this.setData({
-                                      isLocal:true,
-                                      searchCityId:_this.data.searchCityId,
-                                      searchCityName:_this.data.searchCityName,
-                                      currentCityId: city.cityId,
-                                      currentCityName: city.cityName||'',
-                                      lat: lat,
-                                      lng: lng,
-                                  })
-                              } else {
-                                  _this.setData({
-                                      isLocal:false,
-                                      searchCityId:_this.data.searchCityId,
-                                      searchCityName:_this.data.searchCityName,
-                                  });
-                              }
-                              callback();
-                          }
-                      })
-                  },
-                  fail: function(res) {
-                      __wxConfig.debug && console.log('homepage qq map reverseGeocoder fail res = ', res);
-                      _this.setData({
-                          isLocal:false,
-                          searchCityId:_this.data.searchCityId,
-                          searchCityName:_this.data.searchCityName,
-                      });
-                      callback();
-                  }
-              });
-          },
-          fail: function(e) {
-              __wxConfig.debug && console.log('homepage location fail');
-              _this.setData({
-                  isLocal:false,
-                  searchCityId:_this.data.searchCityId,
-                  searchCityName:_this.data.searchCityName,
-              });
-              callback();
-          }
-      })
-    },
+    // getLocationCity: function(hasCurrentSelectCity, callback) {
+    //   var _this = this,
+    //       _api = api;
+    //   var qqMap = app.getQQMap();
+    //   var _app = app;
+    //   var lat, lng;
+    //   api.Location.get({
+    //       success: function(res) {
+    //           __wxConfig.debug && console.log("homepage Location res = ", res)
+    //           lat = res.latitude;
+    //           lng = res.longitude;
+    //           qqMap.reverseGeocoder({
+    //               location: {
+    //                   latitude: res.latitude,
+    //                   longitude: res.longitude
+    //               },
+    //               success: function(res) {
+    //                   __wxConfig.debug && console.log("homepage qq map reverseGeocoder success res = ",res);
+    //                   //遍历城市解析出城市id
+    //                   var currentCityName = res.result.address_component.city//北京市
+    //                   __wxConfig.debug && console.log("homepage qq map reverseGeocoder success currentCityName = ",currentCityName);
+    //                   _app.getSavedCityData(function(result){
+    //                       __wxConfig.debug && console.log("homepage qq map reverseGeocoder success getSavedCityData  result = ",result);
+    //                       if(result && result.data && !Util.isEmptyObject(result.data)) {
+    //                           var city = _this.getCityByName(currentCityName.replace(/市/,''), result.data.cityList);
+    //                           isChinaCity = true;
+    //                           if (Util.isEmptyObject(city)) {
+    //                             isChinaCity = false;
+    //                             city = _this.getCityByName(currentCityName.replace(/市/,''), result.data.overseasCityList);
+    //                           }
+    //                           __wxConfig.debug && console.log("homepage qq map reverseGeocoder success getSavedCityData  city = ",city);
+    //                           if(city && city.cityId) {
+    //                               showLocationDailog = city.cityId != _this.data.searchCityId;
+    //                               _this.setData({
+    //                                   isLocal:true,
+    //                                   searchCityId:_this.data.searchCityId,
+    //                                   searchCityName:_this.data.searchCityName,
+    //                                   currentCityId: city.cityId,
+    //                                   currentCityName: city.cityName||'',
+    //                                   lat: lat,
+    //                                   lng: lng,
+    //                               })
+    //                           } else {
+    //                               _this.setData({
+    //                                   isLocal:false,
+    //                                   searchCityId:_this.data.searchCityId,
+    //                                   searchCityName:_this.data.searchCityName,
+    //                               });
+    //                           }
+    //                           callback();
+    //                       }
+    //                   })
+    //               },
+    //               fail: function(res) {
+    //                   __wxConfig.debug && console.log('homepage qq map reverseGeocoder fail res = ', res);
+    //                   _this.setData({
+    //                       isLocal:false,
+    //                       searchCityId:_this.data.searchCityId,
+    //                       searchCityName:_this.data.searchCityName,
+    //                   });
+    //                   callback();
+    //               }
+    //           });
+    //       },
+    //       fail: function(e) {
+    //           __wxConfig.debug && console.log('homepage location fail');
+    //           _this.setData({
+    //               isLocal:false,
+    //               searchCityId:_this.data.searchCityId,
+    //               searchCityName:_this.data.searchCityName,
+    //           });
+    //           callback();
+    //       }
+    //   })
+    // },
     // 根据城市名获取城市信息
-    getCityByName(name, cityList){
-      if(Util.isTxtNotEmpty(name) && !Util.isEmptyObject(cityList)){
-        let city;
-              for(var index = 0; index < cityList.length;index++){
-                  var tempCityList = cityList[index].cityList;
-                  if(!Util.isEmptyObject(tempCityList)) {
-                      for(var j = 0; j<tempCityList.length;j++){
-                          if(Util.isTxtNotEmpty(tempCityList[j].cityName) && null != tempCityList[j].cityName.match(name)) {
-                              return tempCityList[j];
-                          }
-                      }
-                  }
-              }
-      }
-          return null;
-    },
-    onLoad: function (options) {
-      //处理别的页面传递过来的数据
-      __wxConfig.debug && console.log("homepage onLoad")
-      var that = this,
-          _api = api;
-      mRefresh = false
-      that.setData({
-        scrollHeight:getApp().getAppSystemInfo().windowHeight
-      })
-      var pages = getCurrentPages();
-      currentPage = pages[pages.length - 1];
-      currentPage.setLoading(true)
-      app.getSavedCityData(function (result) {
-        if (Util.isEmptyObject(result) || (Util.isEmptyObject(result.data.cityList) &&
-          Util.isEmptyObject(result.data.overseasCityList))) {
-          that.getCityDataFromServer();
-        }
-      });
-      this.loadData();
-    },
+    // getCityByName(name, cityList){
+    //   if(Util.isTxtNotEmpty(name) && !Util.isEmptyObject(cityList)){
+    //     let city;
+    //           for(var index = 0; index < cityList.length;index++){
+    //               var tempCityList = cityList[index].cityList;
+    //               if(!Util.isEmptyObject(tempCityList)) {
+    //                   for(var j = 0; j<tempCityList.length;j++){
+    //                       if(Util.isTxtNotEmpty(tempCityList[j].cityName) && null != tempCityList[j].cityName.match(name)) {
+    //                           return tempCityList[j];
+    //                       }
+    //                   }
+    //               }
+    //           }
+    //   }
+    //       return null;
+    // },
+    // onLoad: function (options) {
+    //   //处理别的页面传递过来的数据
+    //   __wxConfig.debug && console.log("homepage onLoad")
+    //   var that = this,
+    //       _api = api;
+    //   mRefresh = false
+    //   that.setData({
+    //     // scrollHeight:getApp().getAppSystemInfo().windowHeight
+    //   })
+    //   var pages = getCurrentPages();
+    //   currentPage = pages[pages.length - 1];
+    //   currentPage.setLoading(true)
+      // app.getSavedCityData(function (result) {
+      //   if (Util.isEmptyObject(result) || (Util.isEmptyObject(result.data.cityList) &&
+      //     Util.isEmptyObject(result.data.overseasCityList))) {
+      //     that.getCityDataFromServer();
+      //   }
+      // });
+    //   this.loadData();
+    // },
     loadData:function(){
       //获取当前选择的城市
       __wxConfig.debug && console.log("homepage loadData")
@@ -194,82 +194,82 @@ var tabNames = ["recommand", "nearby"];
         })
       })
     },
-    onShow: function () {
-      // 检测登陆是否失效，并尝试登陆
-      var _this = this
-      __wxConfig.debug && console.log("homepage onShow")
-      token.checkSessionToken(function (isok) {
-        __wxConfig.debug && console.log("homepage checkSessionToken isok = ", isok)
-        if (isok) {
-          _this.showLocationDailog()
-        } else {
-          token.autoLogin(function () {
-            api.showModal({
-              title: '授权登录',
-              content: '登录后艺龙将获得您的昵称、头像等公开信息。',
-              showCancel: true,
-              confirm: function () {
-                token.autoLoginConfirm(_this.loginCallback.bind(_this))
-              }.bind(_this),
-              cancel: function () {
-                token.autoLoginCancel()
-              }
-            })
-          }.bind(_this), _this.loginCallback.bind(_this))
-        }
-      }.bind(_this))
-    },
-    showLocationDailog:function(){
-      var _api = api, that = this;
-      __wxConfig.debug && console.log("homepage showLocationDailog showLocationDailog = ", showLocationDailog)
-      if(showLocationDailog) {
-        showLocationDailog = false
-        if(Util.isTxtNotEmpty(that.data.currentCityName)){
-          _api.showModal({
-            title: '',
-            content: '系统检测到您当前的城市为'+that.data.currentCityName+',是否要更换城市?',
-            showCancel: true,
-            confirm: function () {
-              that.setData({
-                searchCityId:that.data.currentCityId,
-                searchCityName:that.data.currentCityName,
-              })
-              Util.saveLocationChooseCity(_api, {cityId:that.data.currentCityId,cityName:that.data.currentCityName}, isChinaCity)
-              isChinaCity = false;
-              that.getHomePageData()
-            }.bind(that),
-            cancel: function () {
-              that.showNoDataLocation()
-            }
-          })
-        }
-      } else {
-        that.showNoDataLocation()
-      }
-    },
-    showNoDataLocation:function(){
-      //当前城市无数据弹框
-      __wxConfig.debug && console.log("homepage showNoDataLocation showNoDataDialog = ", showNoDataDialog)
-      if (showNoDataDialog) {
-        var _api = api, that = this;
-        showNoDataDialog = false
-        _api.showModal({
-            title: '',
-            content: '很抱歉, 您当前的城市'+that.data.searchCityName+'暂无数据,请选择其他城市试试',
-            showCancel: false,
-            confirm: function () {
-              //跳转城市选择页
-              that.onCitySelectClick();
-            }.bind(that)
-        })
-      }
-    },
-    getCityDataFromServer: function () {
-      var _app = app;
-      CityService.GetCityData().then(function (data) {
-        _app.saveCityData(data);
-      }.bind(this));
-    },
+    // onShow: function () {
+    //   // 检测登陆是否失效，并尝试登陆
+    //   var _this = this
+    //   __wxConfig.debug && console.log("homepage onShow")
+    //   token.checkSessionToken(function (isok) {
+    //     __wxConfig.debug && console.log("homepage checkSessionToken isok = ", isok)
+      //   if (isok) {
+      //     _this.showLocationDailog()
+      //   } else {
+      //     token.autoLogin(function () {
+      //       api.showModal({
+      //         title: '授权登录',
+      //         content: '登录后艺龙将获得您的昵称、头像等公开信息。',
+      //         showCancel: true,
+      //         confirm: function () {
+      //           token.autoLoginConfirm(_this.loginCallback.bind(_this))
+      //         }.bind(_this),
+      //         cancel: function () {
+      //           token.autoLoginCancel()
+      //         }
+      //       })
+      //     }.bind(_this), _this.loginCallback.bind(_this))
+      //   }
+      // }.bind(_this))
+    // },
+    // showLocationDailog:function(){
+    //   var _api = api, that = this;
+    //   __wxConfig.debug && console.log("homepage showLocationDailog showLocationDailog = ", showLocationDailog)
+    //   if(showLocationDailog) {
+    //     showLocationDailog = false
+    //     if(Util.isTxtNotEmpty(that.data.currentCityName)){
+    //       _api.showModal({
+    //         title: '',
+    //         content: '系统检测到您当前的城市为'+that.data.currentCityName+',是否要更换城市?',
+    //         showCancel: true,
+    //         confirm: function () {
+    //           that.setData({
+    //             searchCityId:that.data.currentCityId,
+    //             searchCityName:that.data.currentCityName,
+    //           })
+    //           Util.saveLocationChooseCity(_api, {cityId:that.data.currentCityId,cityName:that.data.currentCityName}, isChinaCity)
+    //           isChinaCity = false;
+    //           that.getHomePageData()
+    //         }.bind(that),
+    //         cancel: function () {
+    //           that.showNoDataLocation()
+    //         }
+    //       })
+    //     }
+    //   } else {
+    //     that.showNoDataLocation()
+    //   }
+    // },
+    // showNoDataLocation:function(){
+    //   //当前城市无数据弹框
+    //   __wxConfig.debug && console.log("homepage showNoDataLocation showNoDataDialog = ", showNoDataDialog)
+    //   if (showNoDataDialog) {
+    //     var _api = api, that = this;
+    //     showNoDataDialog = false
+    //     _api.showModal({
+    //         title: '',
+    //         content: '很抱歉, 您当前的城市'+that.data.searchCityName+'暂无数据,请选择其他城市试试',
+    //         showCancel: false,
+    //         confirm: function () {
+    //           //跳转城市选择页
+    //           that.onCitySelectClick();
+    //         }.bind(that)
+    //     })
+    //   }
+    // },
+    // getCityDataFromServer: function () {
+    //   var _app = app;
+    //   CityService.GetCityData().then(function (data) {
+    //     _app.saveCityData(data);
+    //   }.bind(this));
+    // },
     getHomePageData: function () {
       var that = this, _api = api;
       __wxConfig.debug && console.log("homepage getHomePageData")
